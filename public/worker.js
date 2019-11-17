17d683845c1e3c3168b8f077f0e943856788ac88
Dylan O'Driscoll <dylan.odriscoll@gmail.com>
2019-11-17 17:27:08 +0000

    make pwa work

--- public/worker.js
+++ public/worker.js
@@ -1,29 +1,47 @@
-importScripts("https://d3js.org/d3-collection.v1.min.js");
-importScripts("https://d3js.org/d3-dispatch.v1.min.js");
-importScripts("https://d3js.org/d3-quadtree.v1.min.js");
-importScripts("https://d3js.org/d3-timer.v1.min.js");
-importScripts("https://d3js.org/d3-force.v1.min.js");
+var CACHE_NAME = 'cse-wfh';
+var urlsToCache = [
+  '/',
+];
 
-onmessage = function(event) {
-  var nodes = event.data.nodes,
-      links = event.data.links;
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-width = 1200
-height = 660
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
 
-    var simulation = d3.forceSimulation(nodes)
-        .force("charge", d3.forceManyBody().strength(-80).distanceMax(100))
-        .force("link", d3.forceLink(links).id(function(d) { return d.id; }).distance(20).strength(function(d) { return d.weight/20 }))
-        .force("center", d3.forceCenter(width / 2, height / 2))
-        .force("y", d3.forceY(height / 2))
-        .force("x", d3.forceY(width / 2))
-        .stop();
-
-
-  for (var i = 0, n = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay())); i < n; ++i) {
-    postMessage({type: "tick", progress: i / n});
-    simulation.tick();
-  }
-
-  postMessage({type: "end", nodes: nodes, links: links});
-};
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.forEach(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
